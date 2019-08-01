23569c945809f5ff03ef1b5beb84e5a36472438b
OfficialKingJulian <contact@julians.email>
2019-08-01 08:54:15 +0000

    changed config.fish mainly

--- env/config.fish
+++ env/config.fish
@@ -1,26 +1,34 @@
-function fish_prompt
-    set_color $fish_color_cwd
-    echo -n (prompt_pwd)
-    set_color blue
-    echo -n '    > '
-end
-set fish_greeting
-function mkpdf 
-  markdown-pdf
-end
-function cls
-  clear
-end
-function mk 
-	mkdir $argv
-	cd $argv
-end
-function cointop
-  snap run cointop
-end
-function weather
-  curl http://wttr.in/southampton
-end
-export VISUAL=vim
-export EDITOR="$VISUAL"
-export PATH="~/dotfiles/:$PATH"
+# Fish Config
+  function fish_prompt
+      set_color $fish_color_cwd
+      set_color yellow
+      echo -n '$ '
+      set_color blue
+      echo -n 'Julian-Server '
+      set_color yellow
+      echo -n '@ '
+      set_color purple
+      echo -n (prompt_pwd)
+      set_color yellow
+      echo -n ' > '
+  end
+  set fish_greeting
+  function mkpdf 
+    markdown-pdf
+  end
+  function cls
+    clear
+  end
+  function mk 
+    mkdir $argv
+    cd $argv
+  end
+  function cointop
+    snap run cointop
+  end
+  function weather
+    curl http://wttr.in/southampton
+  end
+  export VISUAL=vim
+  export EDITOR="$VISUAL"
+  export PATH="~/dotfiles/:$PATH"
