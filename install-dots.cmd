f6d76448d01859cc676fed721122b1debffcbc46
Julian Orchard <jorchard@wessex.co.uk>
2019-08-21 10:48:02 +0100

    Finished install-dots.cmd

--- install-dots.cmd
+++ install-dots.cmd
@@ -2,5 +2,16 @@
 
 :: Installing some of these elements
 :: on Windows (mainly for vim)
+ 
+:: VIM
+   mkdir ..\.vim
+   xcopy vim\* ..\.vim\
 
+:: BASH
+   xcopy env\bashrc* ..\bashrc*
+   ren ..\bashrc .bashrc
+
+:: MINTTY
+   xcopy env\minttyrc* ..\minttyrc*
+   ren ..\minttyrc .minttyrc
 
