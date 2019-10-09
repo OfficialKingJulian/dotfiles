e8115d69452bbb0dc2174b9440e75492afd6a850
OfficialKingJulian <julianorchard@hotmail.com>
2019-10-09 10:00:00 +0100

    added aliases for neomutt, mutt

--- env/config.fish
+++ env/config.fish
@@ -9,6 +9,9 @@
       echo -n '@ '
       set_color purple
       echo -n (prompt_pwd)
+      set -l git_branch (git branch ^/dev/null | sed -n '/\* /s///p')
+      set_color red
+      echo -n " $git_branch"
       set_color yellow
       echo -n ' > '
   end
@@ -29,6 +32,16 @@
   function weather
     curl http://wttr.in/southampton
   end
+  function mutt
+    neomutt
+  end
+  function :q
+    exit
+  end
   export VISUAL=vim
   export EDITOR="$VISUAL"
   export PATH="~/dotfiles/:$PATH"
+
+# Bindings
+
+bind :q 'exit'
